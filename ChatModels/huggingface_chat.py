d2460b3c364a9639c19efa79de7ab391b559ba0c
anshul <anshulgupta2028@gmail.com>
2026-06-28 14:52:01 +0530

    Add Hugging Face chat model implementation and create localModel.py

--- ChatModels/huggingface_chat.py
+++ ChatModels/huggingface_chat.py
@@ -1,2 +1,15 @@
 from dotenv import load_dotenv
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+load_dotenv(override=True)
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Flash",
+    temperature=0.7,
+    # max_length=1024,
+)
+
+model = ChatHuggingFace(llm=llm)
+response = model.invoke("Who are you ?")
+print("model--->", response.content)
+
