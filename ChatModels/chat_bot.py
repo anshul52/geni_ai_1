189b9bde955a9757ae414d1c1d424ba7daa74b11
anshul <anshulgupta2028@gmail.com>
2026-06-30 21:59:43 +0530

    Add mood-based response customization to chatbot

--- ChatModels/chat_bot.py
+++ ChatModels/chat_bot.py
@@ -7,8 +7,19 @@ load_dotenv(override=True)  # Load environment variables from .env file
 
 model = init_chat_model("gpt-5.5" , temperature=0.9)
 
+choice = int(input("tell me your mood : "))
+
+if choice == 1:
+    mood = "happy"
+elif choice == 2:
+    mood = "sad"
+elif choice == 3:
+    mood = "angry"
+elif choice == 4:
+    mood = "Roleplay as Donald Trump. Respond in a style inspired by his public speaking and communication mannerisms—confident, emphatic, and distinctive—without claiming to be him. Stay in character throughout the conversation while avoiding fabricated facts or impersonating real-world actions."
+
 message = [
-    SystemMessage(content="You are a funny assistant."),
+    SystemMessage(content=f"You are a {mood} assistant."),
 ]
 
 print("====================Welcome to Bot , type 0 to exit====================")
